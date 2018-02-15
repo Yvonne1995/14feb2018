446b4e6a14fdc656c07fcf20a8e6e9a8eb4d23cf
Yvonne1995 <ykagondu@gmail.com>
2018-02-15 12:24:25 +0300

    jhfd

---
+++ js/script5.js
@@ -0,0 +1,12 @@
+
+      $(document).ready(function() {
+
+        $("div#click-one").click(function(event) {
+          whatToSay = "Hello!";
+          alert(whatToSay);
+        });
+
+        $("div#click-two").click(function(event) {
+          alert(whatToSay);
+        });
+      });
