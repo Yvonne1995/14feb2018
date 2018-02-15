45e741097878c17765b98fafe35ec31e58f3afda
Yvonne1995 <ykagondu@gmail.com>
2018-02-15 11:38:22 +0300

    DOM init

---
+++ js/script3.js
@@ -0,0 +1,21 @@
+var userSentence=prompt("Enter a sentence:");
+
+//first function
+var capitalizedLetters;
+
+
+function capitalizeletters(){
+capitalizeletters = userSentence.charAt(0).toUpperCase() + userSentence
+}
+capitalizeletters();
+console.log(outputOne)
+//Second function
+function reverseLetters(){
+  return outputOne.split("").reverse().join("");
+}
+
+//Third function
+functin callBoth(outputOne) {
+  capitalizeletters();
+}
+console.log(reverseLetters());
